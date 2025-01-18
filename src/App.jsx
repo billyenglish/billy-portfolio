43454f3c0debc0ed2ab30bca14464b88762dee45
Demar English <demarenglish@Mac.attlocal.net>
2025-01-18 16:32:38 -0500

    Added the navigation component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import Nav from "./components/Nav";
-import Footer from "./components/Footer";
+import Main from "./components/Main";
 
 function App() {
 
   return (
     <>
       <Nav />
-      <Footer />
+      <Main />
     </>
   )
 }
