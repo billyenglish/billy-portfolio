dadf8d587951cae2f37e4ac39b5e96d104e81ce9
Demar English <demarenglish@Mac.attlocal.net>
2025-01-18 16:36:50 -0500

    Adjusted the nav list to use the mapping function.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,19 +1,41 @@
+const navItems = [
+    {
+        navTitle: "Home",
+        navKey: "home"
+    },
+    {
+        navTitle: "About",
+        navKey: "about"
+    },
+    {
+        navTitle: "Projects",
+        navKey: "projects"
+    },
+    {
+        navTitle: "Skills",
+        navKey: "skills"
+    },
+    {
+        navTitle: "Contact Me",
+        navKey: "contact me"
+    }
+];
+
+
 const Nav = () => {
     return (
         <>
             <header>
                 <nav className="nav-menu">
                     <div className="logo">
-                        <h1 id="logo">Billy E</h1>
+                        <h1 id="logo">B E</h1>
                     </div>
 
                     <div className="nav-container">
                         <ul className="nav-list">
-                            <li className="nav-item">Home</li>
-                            <li className="nav-item">About</li>
-                            <li className="nav-item">Projects</li>
-                            <li className="nav-item">Skills</li>
-                            <li className="nav-item">Contact Me</li>
+                            {navItems.map((navItem, navKeys) => {
+                                return <li className="nav-time" key={navKeys.navKeys}>{navItem.navTitle}</li>
+                            })}
                         </ul>
                     </div>
                 </nav>
