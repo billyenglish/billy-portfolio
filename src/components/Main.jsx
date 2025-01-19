274af4a78a63eb1166ef52e70ef12531da8d7b72
Demar English <demarenglish@Mac.attlocal.net>
2025-01-19 01:50:19 -0500

    Changed the herosection classes to ids.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,13 @@
 const Main = () => {
     return (
-        <main>
+        <main className="hero-section">
+            <section>
+                <h2 id="portfolio-author">Billy English</h2>
+
+                <h3 id="career-title">Software Engineer | Front-End Developer</h3>
+
+                <p id="author-heading">Welcome to my portfolio</p>
+            </section>
         </main>
     );
 };
