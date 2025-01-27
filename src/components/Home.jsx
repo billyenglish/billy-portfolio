975954c48f23d7aba74004fa4a6b3a2e972861cd
Billy English <billyenglish6212@outlook.com>
2025-01-27 10:41:31 -0500

    Created Home Component.

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+const Home = () => {
+    return (
+        <>
+            <section className="section-home">
+                <h2 className="author-title">Billy English</h2>
+
+                <h3 className="professional-title">Software Engineer | Front End Developer</h3>
+            </section>
+        </>
+    );
+};
+
+export default Home;
