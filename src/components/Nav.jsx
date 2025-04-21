0eadd9560e969282cb885020cae3cc6d3e7a7851
Billy English <billyenglish6212@outlook.com>
2025-04-20 21:14:40 -0400

    Adding hamburger menu interactivity.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { GiHamburgerMenu } from 'react-icons/gi';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
 const navLinks = [
     {linkName: "Home", id: 0},
@@ -10,6 +13,21 @@ const navLinks = [
 ];
 
 const Nav = () => {
+
+    const [openMenu, setOpenMenu] = useState(false);
+
+    const handleMenuToggle = () => {
+
+        if (openMenu) {
+            console.log("Menu closed");
+        } else {
+            console.log("Menu opened");
+        }
+
+        setOpenMenu(!openMenu);
+
+    }
+
     return (
         <nav className="nav-container">
             <div className="nav-logo-container">
@@ -17,16 +35,16 @@ const Nav = () => {
                 <p className="professional-logo">Software Engineer</p>
             </div>
 
-            <div className="nav-list-container">
+            <div className={`nav-list-container ${openMenu ? "active" : ""}`}>
                 <ul className="nav-list">
                     {navLinks.map((link) => {
-                        return <li key={link.id}>{link.linkName}</li> 
+                        return <li key={link.id}><a href="/">{link.linkName}</a></li> 
                     })}
                 </ul>
             </div>
 
-            <div className="ham-menu" onClick={() => alert("Hello World")}>
-                <GiHamburgerMenu className="ham-icon" />
+            <div onClick={handleMenuToggle} className="ham-menu">
+                {openMenu ? <FontAwesomeIcon icon={faXmark} /> : <FontAwesomeIcon icon={faBars} />}
             </div>
         </nav>
     );
