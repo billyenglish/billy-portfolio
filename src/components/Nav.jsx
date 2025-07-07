fc3f4c2f40c989f1d045f057d0c243cd103e0feb
Demar English <demarenglish@Mac.attlocal.net>
2025-07-07 12:39:03 -0400

    Adjustted the hamburger menu.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,8 +2,16 @@ import "../assets/styles/nav.css";
 import "../media_queries/nav_media_query.css";
 import { FaBars, FaCode } from 'react-icons/fa';
 import { MdOutlineLightMode } from "react-icons/md";
+import { FaX } from "react-icons/fa6";
+import { useState } from "react";
 
 const Nav = () => {
+
+    const [openMenu, setOpenMenu] = useState(false);
+
+    const toggleMenuOpen = () => {
+        setOpenMenu(openMenu => !openMenu);
+    }
 
     const navItems = [
         {
@@ -38,11 +46,14 @@ const Nav = () => {
 
             <div className="nav-desktop-container">
                 <ul className="nav-desktop-list">
-                    {navItems.map((item) => (
-                        <li className="nav-desktop-item" key={item.id}>
+                    {navItems.map((items) => (
+                        <li
+                            className="nav-desktop-item"
+                            key={items.id}
+                        >
                             <a href="/">
                                 <FaCode size={16} />
-                                {item.name}
+                                {items.name}
                             </a>
                         </li>
                     ))}
@@ -53,8 +64,24 @@ const Nav = () => {
                 </div>
             </div>
 
-            <div className="hamburger-menu" onClick={() => alert("Hello World")}>
-                <FaBars />
+            <div className={`nav-mobile-container ${openMenu ? "show" : ""}`}>
+                <ul className="nav-mobile-list">
+                    {navItems.map((items) => (
+                        <li
+                            className="nav-mobile-item"
+                            key={items.id}
+                        >
+                            <FaCode size={20} />
+                            {items.name}
+                         </li>
+                    ))}
+                </ul>
+            </div>
+
+            <div className="hamburger-menu" onClick={toggleMenuOpen}>
+                {
+                    !openMenu ? <FaBars size={24} /> : <FaX size={22} />
+                }
             </div>
         </nav>
     );
