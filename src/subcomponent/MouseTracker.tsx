df38912fd00996de2de535bc6472620244eafc63
Demar English <demarenglish@Demars-MacBook-Pro.local>
2025-11-26 03:20:28 -0500

    Wiped the MouseTracker file.

--- src/subcomponent/MouseTracker.tsx
+++ src/subcomponent/MouseTracker.tsx
@@ -1,22 +1,4 @@
-import { useState, useEffect, createContext, useContext } from "react";
-
 const MouseTracker = () => {
-
-    interface MousePosition {
-        x: number;
-        y: number;
-    }
-
-    const [mouseTracker, setMouseTracker] = useState<MousePosition>({ x: 0, y: 0 });
-
-    const handleMousePosition = (e: MouseEvent) => {
-        useEffect(() => {
-            setMouseTracker({
-                x: e.clientX,
-                y: e.clientY
-            })
-        })
-    }
 
     return (
         <>
