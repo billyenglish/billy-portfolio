229b635f91c5eea6c3813f44ed4230b6853ab318
Demar English <demarenglish@Demars-MacBook-Pro.local>
2025-11-26 02:49:19 -0500

    Created Contact Me form.

--- src/component/ContactMe.tsx
+++ src/component/ContactMe.tsx
@@ -1,7 +1,50 @@
+import Nav from "./Nav";
+import Footer from "./Footer";
+import "../assets/styles/contact_me.css";
+import Button from "../subcomponent/Button";
+
 const ContactMe = () => {
 
     return (
         <>
+            <Nav />
+            <section id="contact_me_section" className="main_section">
+                <h3 className="section_title">Contact Me</h3>
+
+                <div id="form_container">
+                    <div>
+                        <input
+                            type="text"
+                            name="full_name"
+                            id="full_name"
+                            placeholder="Name"
+                        />
+                    </div>
+                    <div>
+                        <input
+                            type="text"
+                            name="subject_title"
+                            id="subject_title"
+                            placeholder="Subject Title"
+                        />
+                    </div>
+                    <div>
+                        <textarea placeholder="Details">
+                        </textarea>
+                    </div>
+                    <div className="form_control_panel">
+                        <Button
+                            text="Clear"
+                            className="form_button"
+                        />
+                        <Button
+                            text="Submit"
+                            className="form_button"
+                        />
+                    </div>
+                </div>
+            </section>
+            <Footer />
         </>
     );
 };
--- src/component/Projects.tsx
+++ src/component/Projects.tsx
@@ -1,7 +1,85 @@
+import Nav from "./Nav";
+import Footer from "./Footer";
+import Button from "../subcomponent/Button";
+import "../assets/styles/projects.css";
+
 const Projects = () => {
 
     return (
         <>
+            <Nav />
+            <section id="projects_section" className="main_section">
+                <h3 className="section_title">
+                    My Projects
+                </h3>
+
+                <div id="project_container">
+                    <div className="project_card">
+                        <h4 className="card_title">
+                            Project Title
+                        </h4>
+
+                        <h5 className="card_subtitle">
+                            <span className="tech_stack">
+                                Tech Stack:
+                            </span> Tech Stack: HTML, CSS, JavaScript, React, Tailwind CSS
+                        </h5>
+
+                        <p className="project_description">
+                            <span></span> A responsive single-page e-commerce application that
+                            allows users to browse products, view detailed descriptions, and
+                            add items to a shopping cart. Integrated interactive UI components,
+                            mobile-first design, and basic cart functionality using React state
+                            management.
+                        </p>
+
+                        <div className="project_control_panel">
+                            <Button
+                                text="Source Code"
+                                className="project_button"
+                            />
+
+                            <Button
+                                text="Live Demo"
+                                className="project_button"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="project_card">
+                        <h4 className="card_title">
+                            Project Title
+                        </h4>
+
+                        <h5 className="card_subtitle">
+                            <span className="tech_stack">
+                                Tech Stack:
+                            </span> Tech Stack: HTML, CSS, JavaScript, React, Tailwind CSS
+                        </h5>
+
+                        <p className="project_description">
+                            <span></span> A responsive single-page e-commerce application that
+                            allows users to browse products, view detailed descriptions, and
+                            add items to a shopping cart. Integrated interactive UI components,
+                            mobile-first design, and basic cart functionality using React state
+                            management.
+                        </p>
+
+                        <div className="project_control_panel">
+                            <Button
+                                text="Source Code"
+                                className="project_button"
+                            />
+
+                            <Button
+                                text="Live Demo"
+                                className="project_button"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <Footer />
         </>
     );
 };
