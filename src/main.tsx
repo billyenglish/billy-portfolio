e254e5a1e2d46414dbceb1e4f0b6d913016ab01f
Demar English <demarenglish@Demars-MacBook-Pro.local>
2025-12-08 17:45:10 -0500

    Added wrap context provider to the main component.

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { HashRouter } from 'react-router-dom'
+import ThemeProvide from "./subcomponent/ThemeProvider"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HashRouter>
-      <App />
+      <ThemeProvide>
+        <App />
+      </ThemeProvide>
     </HashRouter>
   </StrictMode>,
 )
