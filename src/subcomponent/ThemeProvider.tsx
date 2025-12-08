f2b1b889d8289d06e9e2bae9fc295d937c68f7cd
Demar English <demarenglish@Demars-MacBook-Pro.local>
2025-12-08 17:18:40 -0500

    Created Theme Provider.

---
+++ src/subcomponent/ThemeProvider.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState, type ReactNode } from "react";
+import { ThemeContext, type ThemeContextType } from "./ThemeContext";
+
+interface ThemeProviderProps {
+  children: ReactNode;
+}
+
+const ThemeProvider = ({ children }: ThemeProviderProps) => {
+  const [theme, setTheme] = useState<"light" | "dark">("light");
+
+  const toggleTheme = () => {
+    setTheme(prev => (prev === "light" ? "dark" : "light"));
+  };
+
+  useEffect(() => {
+    document.body.dataset.theme = theme;
+  }, [theme])
+
+  const value: ThemeContextType = {
+    theme,
+    toggleTheme,
+  };
+
+  return (
+    <ThemeContext.Provider value={value}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeProvider;
