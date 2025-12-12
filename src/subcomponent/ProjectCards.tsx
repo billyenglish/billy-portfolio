fb9fb2c715b23eba82af0ee7b38629a9661ec78d
Demar English <demarenglish@Demars-MacBook-Pro.local>
2025-12-11 19:17:27 -0500

    Updated the button components in the project card section to links.

--- src/subcomponent/ProjectCards.tsx
+++ src/subcomponent/ProjectCards.tsx
@@ -1,11 +1,22 @@
-import Button from "../subcomponent/Button";
+import CardButtonLinks from "./CardButtonLinks"
 import type { FC } from "react";
 
 type CardProps = {
     title: string;
     subtitle: string;
     description: string;
+    className: string;
+    sourceCode: string;
+    href: string;
 }
+
+const SourceCodeLinks = [
+    { linkText: 'Source Code', href: "/", id: 0 }
+];
+
+const LiveDemoLinks = [
+    { linkText: 'Live Code', href: "/", id: 0}
+]
 
 const ProjectCard: FC<CardProps> = ({ title, subtitle, description, }) => {
 
@@ -28,15 +39,27 @@ const ProjectCard: FC<CardProps> = ({ title, subtitle, description, }) => {
             </p>
 
             <div className="project_control_panel">
-                <Button
-                    text="Source Code"
-                    className="project_button"
-                />
+                {
+                    SourceCodeLinks.map((currentLinks) => (
+                        <CardButtonLinks
+                            linkText="Source Code"
+                            href={currentLinks.href}
+                            className="project_button"
+                        />
+                    ))
 
-                <Button
-                    text="Live Demo"
-                    className="project_button"
-                />
+
+                }
+
+                {
+                    LiveDemoLinks.map((currentLinks) => (
+                        <CardButtonLinks
+                            linkText="Live Demo"
+                            href={currentLinks.href}
+                            className="project_button"
+                        />
+                    ))
+                }
             </div>
         </div>
     );
